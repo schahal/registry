39b264a7f979d4df90b13f2a9681611031d77313
Michael Smith <throwawayclover@gmail.com>
2025-04-14 13:48:19 +0000

    refactor: split up package boundaries

--- scripts/contributors/contributors.go
+++ scripts/contributors/contributors.go
@@ -1,15 +1,9 @@
-// This package is for validating all contributors within the main Registry
-// directory. It validates that it has nothing but sub-directories, and that
-// each sub-directory has a README.md file. Each of those files must then
-// describe a specific contributor. The contents of these files will be parsed
-// by the Registry site build step, to be displayed in the Registry site's UI.
 package main
 
 import (
 	"bufio"
 	"errors"
 	"fmt"
-	"log"
 	"net/url"
 	"os"
 	"path"
@@ -448,33 +442,4 @@ func validateRelativeUrls(
 		phase:  "Relative URL validation",
 		errors: problems,
 	}
-}
-
-func main() {
-	log.Println("Starting README validation")
-	allReadmeFiles, err := aggregateContributorReadmeFiles()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	log.Printf("Processing %d README files\n", len(allReadmeFiles))
-	contributors, err := parseContributorFiles(allReadmeFiles)
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Printf(
-		"Processed %d README files as valid contributor profiles",
-		len(contributors),
-	)
-
-	err = validateRelativeUrls(contributors)
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Println("All relative URLs for READMEs are valid")
-
-	log.Printf(
-		"Processed all READMEs in the %q directory\n",
-		rootRegistryPath,
-	)
 }
